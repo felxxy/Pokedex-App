7f911e975f69ee23e59fde1e3f7973c81cdb609f
felxxy <isabellevasconcelos854@gmail.com>
2025-08-18 10:45:18 -0300

    conferir linha 12 do pokemon.jsx da pasta app

--- pokedex/app/index.jsx
+++ pokedex/app/index.jsx
@@ -5,7 +5,7 @@ import Card from "../components/Card";
 
 export default function Index() {
 
-  const renderPokemon = ({ item }) = (
+  const renderPokemon = ({ item }) => (
     <Card pokemon={item}/>
   )
 
